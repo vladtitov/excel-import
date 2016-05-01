a6132b6d2696abc465741e24b31c99277f875930
GuencoYuriy <yrik69@mail.ru>
2016-05-01 21:45:39 +0300

    CSS table

--- js/Main.js
+++ js/Main.js
@@ -11,30 +11,7 @@ var myapp;
                 this[str] = opt[str];
             }
             var plus = $('#btn-plus').click(function () {
-                var input = $('<input type="file">').appendTo(plus.parent()).change(function () {
-                    console.log(input);
-                    var val = input.val();
-                    // $.post(this.url_ret, {file: val}).done((res)=>{
-                    //     console.log(res);
-                    // })
-                    var el = input.get(0);
-                    var files = el.files;
-                    var file = files[0];
-                    var form = new FormData();
-                    form.append('myfile', file);
-                    $.ajax({
-                        url: _this.url_ret,
-                        type: 'POST',
-                        dataType: 'json',
-                        data: form,
-                        cache: false,
-                        contentType: false,
-                        processData: false
-                    }).done(function (res) {
-                        // console.log(res);
-                        _this.loadXLS(res.result);
-                    });
-                });
+                _this.onPlusClick(plus);
             });
         }
         Main.prototype.parseXLS = function (data) {
@@ -45,6 +22,30 @@ var myapp;
             // $.get(url).done((res)=> this.parseXLS(res));
             console.log(url);
             var book = XLS.readFile(url);
+        };
+        Main.prototype.onPlusClick = function (plus) {
+            var _this = this;
+            var input = $('<input type="file">').appendTo(plus.parent()).change(function () {
+                console.log(input);
+                var val = input.val();
+                var el = input.get(0);
+                var files = el.files;
+                var file = files[0];
+                var form = new FormData();
+                form.append('myfile', file);
+                input.remove();
+                $.ajax({
+                    url: _this.url_ret,
+                    type: 'POST',
+                    dataType: 'json',
+                    data: form,
+                    cache: false,
+                    contentType: false,
+                    processData: false
+                }).done(function (res) {
+                    _this.loadXLS(res.result);
+                });
+            });
         };
         return Main;
     }());
--- js/Main.ts
+++ js/Main.ts
@@ -17,7 +17,32 @@ module myapp{
             // $.get(url).done((res)=> this.parseXLS(res));
             console.log(url);
             var book = XLS.readFile(url);
+        }
 
+        onPlusClick(plus) {
+            var input = $('<input type="file">').appendTo(plus.parent()).change(()=>{
+                console.log(input);
+                var val = input.val();
+
+                var el: HTMLInputElement = input.get(0);
+                var files: FileList = el.files;
+                var file: File = files[0];
+                var form: FormData = new FormData();
+                form.append('myfile', file);
+                input.remove();
+
+                $.ajax({
+                    url: this.url_ret,
+                    type: 'POST',
+                    dataType: 'json',
+                    data: form,
+                    cache: false,
+                    contentType: false,
+                    processData: false
+                }).done((res)=>{
+                    this.loadXLS(res.result);
+                })
+            })
         }
 
         constructor(opt:any){
@@ -25,37 +50,8 @@ module myapp{
                 this[str] = opt[str];
             }
             var plus = $('#btn-plus').click(()=>{
-
-                var input = $('<input type="file">').appendTo(plus.parent()).change(()=>{
-                    console.log(input);
-                    var val = input.val();
-                    // $.post(this.url_ret, {file: val}).done((res)=>{
-                    //     console.log(res);
-                    // })
-
-                    var el: HTMLInputElement = input.get(0);
-                    var files: FileList = el.files;
-                    var file: File = files[0];
-                    var form: FormData = new FormData();
-                    form.append('myfile', file);
-                    
-                    $.ajax({
-                        url: this.url_ret,
-                        type: 'POST',
-                        dataType: 'json',
-                        data: form,
-                        cache: false,
-                        contentType: false,
-                        processData: false
-                    }).done((res)=>{
-                        // console.log(res);
-                        this.loadXLS(res.result);
-
-                    })
-                })
+                this.onPlusClick(plus);
             })
         }
     }
-
 }
-
